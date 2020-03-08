8ccb1202e694878f8c055da84c13ff1ece6ec4f9
Andrey <Oksana76>
2020-03-08 12:05:10 +0200

    Added MainConfiguration

---
+++ src/main/java/com/papaya/papayagraphqlspqrpoc/elasticformstarter/config/MainConfiguration.java
@@ -0,0 +1,9 @@
+package com.papaya.papayagraphqlspqrpoc.elasticformstarter.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class MainConfiguration {
+}
